94bf0295f8f4cc20c6706be982f7615a4ef99f8a
fenrir <bstsirano@gmail.com>
2021-08-20 00:07:18 +0900

    first commit

---
+++ src/test/java/com/example/jscript/JscriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
